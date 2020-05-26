637fa9acf013dca24a21b01bfcfd3ee354b71ec5
jesus <jesus@heavens.com>
2020-05-26 20:07:40 +0300

    - updated packages

--- rollup.config.js
+++ rollup.config.js
@@ -6,15 +6,11 @@ export default [{
   input: "tsc/dots-animation.js",
   output: [
     { file: "dist/index.esm.js", format: "es" },
-    { file: "dist/index.esm.min.js", format: "es" },
+    { file: "dist/index.esm.min.js", format: "es", plugins: [terser()] },
     { file: "dist/index.umd.js", format: "umd", name: "dotsAnim" },
-    { file: "dist/index.umd.min.js", format: "umd", name: "dotsAnim" },
+    { file: "dist/index.umd.min.js", format: "umd", name: "dotsAnim", plugins: [terser()] },
   ],
   plugins: [
-    terser({
-      include: [/^.+\.min\.js$/],
-      exclude: ["some*"],
-    }),
     commonjs(),
     resolve({
       browser: true,
